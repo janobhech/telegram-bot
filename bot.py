b6c6fa18941872432de4cfbb6aa4463df796566d
janobhech <janobhech25@gmail.com>
2026-04-14 22:50:54 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,6 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-if__name__ =="__main__":
+if __name__ == "__main__":
     app.run_polling()
 # test 123
