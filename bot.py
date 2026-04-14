92f86ffbd40120feb2620692ca10f136b3133042
janobhech <janobhech25@gmail.com>
2026-04-14 11:24:02 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,4 +15,5 @@ app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-app.run_polling(drop_pending_updates=True)
+if__name__=="__main__":
+    app.run_polling(drop_pending_updates=True)
