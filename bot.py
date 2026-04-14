e25413d83918b2089fcf681366f3a3903da00bb3
janobhech <janobhech25@gmail.com>
2026-04-14 15:24:24 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
-print("Bot ishga tushdi")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
+TOKEN = "8790640164:AAF4l-SBZIY9sVB1BgtgE2KtKils3IRmOGA"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Salom! Men yuridik botman 🤖")
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    await update.message.reply_text(f"Siz yozdingiz: {text}")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+if__name__ =="__main__":
+    app.run_polling()
